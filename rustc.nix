fae975e8c9f88deaa35747aea46349fc1449502e
Bernardo Meurer Costa <bemeurer@amazon.com>
2024-06-06 15:19:38 +0000

    refactor: produce RUST_BOOTSTRAP_CONFIG

--- rustc.nix
+++ rustc.nix
@@ -1,4 +1,5 @@
 { mkShell
+, formats
 , binutils
 , cacert
 , cmake
@@ -6,22 +7,51 @@
 , git
 , glibc
 , ninja
-, nix
 , openssl
 , patchelf
 , pkg-config
 , python3
+, mdbook
+, mdbook-i18n-helpers
+
+, bootstrapConfig ? null
+, extraBootstrapConfig ? { }
 }:
 mkShell {
   name = "rustc";
 
-  nativeBuildInputs =  [
-    binutils cmake ninja pkg-config python3 git curl cacert patchelf nix
+  nativeBuildInputs = [
+    binutils
+    cmake
+    ninja
+    pkg-config
+    python3
+    git
+    patchelf
+
+    # bootstrap downloads
+    curl
+    cacert
+
+    # docs
+    mdbook
+    mdbook-i18n-helpers
   ];
 
   buildInputs = [
-    openssl glibc.out glibc.static
+    openssl
+    glibc.out
+    glibc.static
   ];
+
+  RUST_BOOTSTRAP_CONFIG = (formats.toml { }).generate "config.toml" (
+    if bootstrapConfig != null then bootstrapConfig
+    else
+      ({
+        build.patch-binaries-for-nix = true;
+        llvm.download-ci-llvm = false;
+      } // extraBootstrapConfig)
+  );
 
   # Avoid creating text files for ICEs.
   RUSTC_ICE = "0";
